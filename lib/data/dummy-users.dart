ed9753e0c3570ede17d0d5ba172d1042f5655a17
Eli Marins <edas.marins98@gmail.com>
2021-04-15 20:34:13 -0300

    Criado padrao mvc  do projeto

---
+++ lib/data/dummy-users.dart
@@ -0,0 +1,34 @@
+import 'package:my_contacts/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Pinguim Mutcha Marra',
+      email: 'eli_apelao@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Pinguim Poka Bala',
+      email: 'ibira_apelao@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'O Barão Pinguim',
+      email: 'paim_apelao@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'O Rei Pinguim',
+      email: 'rubao_apelao@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551_960_720.png'),
+  '5': const User(
+      id: '5',
+      name: 'O Faraó Pinguim',
+      email: 'kraycha_apelao@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551_960_720.png'),
+};
