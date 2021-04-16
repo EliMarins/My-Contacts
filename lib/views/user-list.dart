abb8c96c2370c5727b3ca7844783a5b8bfae6cd3
Eli Marins <edas.marins98@gmail.com>
2021-04-15 21:22:09 -0300

    Montado a estrutura de avatar, nome, email e ID

--- lib/views/user-list.dart
+++ lib/views/user-list.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:my_contacts/data/dummy-users.dart';
+import 'package:my_contacts/widgets/user-tile.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final users = {...DUMMY_USERS};
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pinguinos Team'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Pinguinos Team'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        ));
   }
 }
